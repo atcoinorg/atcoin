de7b226cf40ca7ad22f65427b362f7a47eff3a5a
Anton Kalochelitis <developing.w@gmail.com>
2025-12-04 12:49:18 +0200

    1) openssl 3.0.18; 2) Update openssl.mk packages; 3) Update rename to ATCOIN;

--- cmake/module/GenerateSetupNsi.cmake
+++ cmake/module/GenerateSetupNsi.cmake
@@ -1,4 +1,5 @@
 # Copyright (c) 2023-present The Bitcoin Core developers
+# Copyright (c) 2016-2025 The W-DEVELOP developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
@@ -6,13 +7,13 @@ function(generate_setup_nsi)
   set(abs_top_srcdir ${PROJECT_SOURCE_DIR})
   set(abs_top_builddir ${PROJECT_BINARY_DIR})
   set(CLIENT_URL ${PROJECT_HOMEPAGE_URL})
-  set(CLIENT_TARNAME "bitcoin")
-  set(BITCOIN_GUI_NAME "bitcoin-qt")
-  set(BITCOIN_DAEMON_NAME "bitcoind")
-  set(BITCOIN_CLI_NAME "bitcoin-cli")
-  set(BITCOIN_TX_NAME "bitcoin-tx")
-  set(BITCOIN_WALLET_TOOL_NAME "bitcoin-wallet")
-  set(BITCOIN_TEST_NAME "test_bitcoin")
+  set(CLIENT_TARNAME "atcoin")
+  set(BITCOIN_GUI_NAME "atcoin-qt")
+  set(BITCOIN_DAEMON_NAME "atcoind")
+  set(BITCOIN_CLI_NAME "atcoin-cli")
+  set(BITCOIN_TX_NAME "atcoin-tx")
+  set(BITCOIN_WALLET_TOOL_NAME "atcoin-wallet")
+  set(BITCOIN_TEST_NAME "test_atcoin")
   set(EXEEXT ${CMAKE_EXECUTABLE_SUFFIX})
   configure_file(${PROJECT_SOURCE_DIR}/share/setup.nsi.in ${PROJECT_BINARY_DIR}/bitcoin-win64-setup.nsi USE_SOURCE_PERMISSIONS @ONLY)
 endfunction()
